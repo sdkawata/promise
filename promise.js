b84bdec4d33133f29093c5bc6efa0d3554c62e7d
sdkawata <sadaaakikawata@gmail.com>
2018-04-08 00:12:19 +0900

    use class

--- promise.js
+++ promise.js
@@ -2,127 +2,134 @@ const PROMISE_PENDING = 0;
 const PROMISE_RESOLVED = 1;
 const PROMISE_REJECTED = 2;
 
-let MyPromise = function() {
-  this.__state = PROMISE_PENDING;
-  this.__thenQueue = [];
-  this.__handlerRegistered = false;
-  this.__value = null;
-  this.__reason = null;
-}
+class MyPromise {
+  constructor() {
+    this.__state = PROMISE_PENDING;
+    this.__thenQueue = [];
+    this.__handlerRegistered = false;
+    this.__value = null;
+    this.__reason = null;
+  }
 
-MyPromise.prototype.then = function then(onFullfilled, onRejected) {
-  let promise2 = new MyPromise();
-  this.__thenQueue.push({
-    onFullfilled,
-    onRejected,
-    promise: promise2
-  });
-  if (this.__state !== PROMISE_PENDING) {
-    this.__registerHandler();
+  then(onFullfilled, onRejected) {
+    let promise2 = new MyPromise();
+    this.__thenQueue.push({
+      onFullfilled,
+      onRejected,
+      promise: promise2
+    });
+    if (this.__state !== PROMISE_PENDING) {
+      this.__registerHandler();
+    }
+    return promise2;
   }
-  return promise2;
-}
-MyPromise.prototype.__registerHandler = function() {
-  if (this.__handlerRegistered || this.__thenQueue.length === 0) {
-    return;
+
+  __registerHandler() {
+    if (this.__handlerRegistered || this.__thenQueue.length === 0) {
+      return;
+    }
+    this.__handlerRegistered = true;
+    setTimeout(() => {
+      this.__handleThen();
+    }, 0);
   }
-  this.__handlerRegistered = true;
-  setTimeout(() => {
-    this.__handleThen();
-  }, 0);
-}
-//[[Promise]](promise, x)
-MyPromise.resolvePromise = function(promise, x) {
-  if (promise === x) {
-    promise.__reject(new TypeError('trying to resolve promise with itself'));
-    return;
-  }
-  if ((typeof x !== 'object' && typeof x !== 'function') || x === null) {
-    promise.__resolve(x);
-    return;
-  }
-  let then; 
-  try {
-    then = x.then;
-  } catch(e) {
-    promise.__reject(e);
-  }
-  if (typeof then !== 'function') {
-    promise.__resolve(x);
-    return;
-  }
-  var functionCalled = false;
-  try {
-    then.call(
-      x,
-      (y) => {
-        // resolvePromise
-        if (functionCalled) {
-          return;
-        }
-        functionCalled = true;
-        MyPromise.resolvePromise(promise, y);
-      },
-      (r) => {
-        // rejectPromise
-        if (functionCalled) {
-          return;
-        }
-        functionCalled = true;
-        promise.__reject(r);
-      }
-    )
-  } catch(e) {
-    if (!functionCalled) {
+
+  //[[Promise]](promise, x)
+  static resolvePromise(promise, x) {
+    if (promise === x) {
+      promise.__reject(new TypeError('trying to resolve promise with itself'));
+      return;
+    }
+    if ((typeof x !== 'object' && typeof x !== 'function') || x === null) {
+      promise.__resolve(x);
+      return;
+    }
+    let then; 
+    try {
+      then = x.then;
+    } catch(e) {
       promise.__reject(e);
     }
-  }
-}
-MyPromise.prototype.__handleThen = function() {
-  const currentQueue = this.__thenQueue;
-  this.__thenQueue = [];
-  this.__handlerRegistered = false;
-  currentQueue.forEach(({promise, onFullfilled, onRejected}) => {
-    if (this.__state === PROMISE_RESOLVED) {
-      if (typeof onFullfilled === 'function') {
-        try {
-          var x = onFullfilled.call(undefined, this.__value);
-          MyPromise.resolvePromise(promise, x);
-        } catch(e) {
-          promise.__reject(e);
+    if (typeof then !== 'function') {
+      promise.__resolve(x);
+      return;
+    }
+    var functionCalled = false;
+    try {
+      then.call(
+        x,
+        (y) => {
+          // resolvePromise
+          if (functionCalled) {
+            return;
+          }
+          functionCalled = true;
+          MyPromise.resolvePromise(promise, y);
+        },
+        (r) => {
+          // rejectPromise
+          if (functionCalled) {
+            return;
+          }
+          functionCalled = true;
+          promise.__reject(r);
         }
-      } else {
-        promise.__resolve(this.__value);
-      }
-    } else if (this.__state === PROMISE_REJECTED) {
-      if (typeof onRejected === 'function') {
-        try {
-          var x = onRejected.call(undefined, this.__reason);
-          MyPromise.resolvePromise(promise, x);
-        } catch(e) {
-          promise.__reject(e);
-        }
-      } else {
-        promise.__reject(this.__reason);
+      )
+    } catch(e) {
+      if (!functionCalled) {
+        promise.__reject(e);
       }
     }
-  })
-}
-MyPromise.prototype.__resolve = function(value) {
-  if (this.__state !== PROMISE_PENDING) {
-    return;
   }
-  this.__state = PROMISE_RESOLVED;
-  this.__value = value;
-  this.__registerHandler();
-}
-MyPromise.prototype.__reject = function(reason) {
-  if (this.__state !== PROMISE_PENDING) {
-    return;
+
+  __handleThen() {
+    const currentQueue = this.__thenQueue;
+    this.__thenQueue = [];
+    this.__handlerRegistered = false;
+    currentQueue.forEach(({promise, onFullfilled, onRejected}) => {
+      if (this.__state === PROMISE_RESOLVED) {
+        if (typeof onFullfilled === 'function') {
+          try {
+            var x = onFullfilled.call(undefined, this.__value);
+            MyPromise.resolvePromise(promise, x);
+          } catch(e) {
+            promise.__reject(e);
+          }
+        } else {
+          promise.__resolve(this.__value);
+        }
+      } else if (this.__state === PROMISE_REJECTED) {
+        if (typeof onRejected === 'function') {
+          try {
+            var x = onRejected.call(undefined, this.__reason);
+            MyPromise.resolvePromise(promise, x);
+          } catch(e) {
+            promise.__reject(e);
+          }
+        } else {
+          promise.__reject(this.__reason);
+        }
+      }
+    })
   }
-  this.__state = PROMISE_REJECTED;
-  this.__reason = reason;
-  this.__registerHandler();
+
+  __resolve(value) {
+    if (this.__state !== PROMISE_PENDING) {
+      return;
+    }
+    this.__state = PROMISE_RESOLVED;
+    this.__value = value;
+    this.__registerHandler();
+  }
+
+  __reject(reason) {
+    if (this.__state !== PROMISE_PENDING) {
+      return;
+    }
+    this.__state = PROMISE_REJECTED;
+    this.__reason = reason;
+    this.__registerHandler();
+  }
 }
 
 module.exports = MyPromise
